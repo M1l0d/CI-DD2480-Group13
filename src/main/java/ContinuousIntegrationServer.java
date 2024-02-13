e44f5703203cb697f20ec9668134bd54203414ca
Alexander Widman <119602587+AlexWidman@users.noreply.github.com>
2024-02-13 15:02:44 +0100

    test(CITest): add tests for ContinuousIntegrationServer.java
    - Test for deleteDirectory()
    - Test for handlePushEvent()
    - Test for cloneRepository()

--- src/main/java/ContinuousIntegrationServer.java
+++ src/main/java/ContinuousIntegrationServer.java
@@ -1,14 +1,27 @@
-package src.main.java;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jetty.util.log.Log;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.apache.commons.io.IOUtils;
+import org.apache.maven.shared.invoker.DefaultInvocationRequest;
+import org.apache.maven.shared.invoker.DefaultInvoker;
+import org.apache.maven.shared.invoker.InvocationRequest;
+import org.apache.maven.shared.invoker.InvocationResult;
+
+import java.io.File;
 
 /**
  * Skeleton of a ContinuousIntegrationServer which acts as webhook
@@ -32,6 +45,106 @@ public class ContinuousIntegrationServer extends AbstractHandler {
         // 2nd compile the code
 
         response.getWriter().println("CI job done");
+    }
+
+
+    /**
+     * Method that handles the push event by accepting the JSON object and running
+     * methods to clone the repository, compile it and delete the cloned repository
+     * 
+     * @param jsonObject - JSON object containing the push event
+     */
+    public void handlePushEvent(JSONObject jsonObject) {
+        String clonedRepoPath = "src/main/resources/";
+        File clonedRepoFile = new File(clonedRepoPath);
+
+        cloneRepository(jsonObject, clonedRepoFile);
+        compileRepository(clonedRepoPath, clonedRepoFile);
+        deleteDirectory(clonedRepoFile);
+    }
+
+    /**
+     * Method that clones the repository from the JSON object
+     * 
+     * @param jsonObject     - JSON object containing the push event
+     * @param clonedRepoFile - File object representing the cloned repository
+     */
+    private void cloneRepository(JSONObject jsonObject, File clonedRepoFile) {
+        // Get clone url and branch name
+        JSONObject repository = jsonObject.getJSONObject("repository");
+        String cloneUrl = repository.getString("clone_url");
+        String branchName = jsonObject.getString("ref").replace("refs/heads/", "");
+
+        // Cloning the repository
+        try {
+            System.out.println("Cloning repository...");
+            Git.cloneRepository()
+                    .setURI(cloneUrl)
+                    .setDirectory(clonedRepoFile)
+                    .setBranch(branchName)
+                    .call();
+            System.out.println("Repository cloned successfully");
+        } catch (GitAPIException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Method that compiles the repository using maven commands, specifically "clean
+     * install"
+     * 
+     * @param clonedRepoPath - Path to the cloned repository (src/main/resources/)
+     * @param clonedRepoFile - File object representing the cloned repository
+     */
+    public void compileRepository(String clonedRepoPath, File clonedRepoFile) {
+        InvocationRequest invocationRequest = new DefaultInvocationRequest();
+        invocationRequest.setPomFile(new File(clonedRepoPath, "pom.xml")); // pom.xml is the file that contains the
+                                                                           // maven configuration
+        invocationRequest.setBaseDirectory(clonedRepoFile);
+        invocationRequest.setGoals(Collections.singletonList("clean install"));
+
+        DefaultInvoker invoker = new DefaultInvoker(); // Invoker is used to execute the maven commands
+
+        // Excuting the maven commands
+        try {
+            InvocationResult result = invoker.execute(invocationRequest);
+
+            if (result.getExitCode() == 0) {
+                System.out.println("Build successful!");
+            } else {
+                System.out.println("Build failed. Exit code: " + result.getExitCode());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Method that deletes the cloned repository after the CI job is done to avoid
+     * clutter
+     * The method works recursively by deleting all files and directories within the
+     * cloned repository
+     * 
+     * @param directory - File object representing the cloned repository
+     */
+    public void deleteDirectory(File directory) {
+        try {
+            if (directory.exists()) {
+                File[] files = directory.listFiles();
+                if (files != null) {
+                    for (File file : files) {
+                        if (file.isDirectory()) {
+                            deleteDirectory(file);
+                        } else {
+                            file.delete();
+                        }
+                    }
+                }
+                directory.delete();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     // used to start the CI server in command line
--- src/test/java/temporaryTest.java
+++ src/test/java/temporaryTest.java
@@ -1,10 +1,73 @@
-package src.test.java;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.PrintStream;
+
+import org.json.JSONObject;
 
 public class temporaryTest {
+    private ContinuousIntegrationServer CIServer;
+
+    @Before
+    public void setUp(){
+        CIServer = new ContinuousIntegrationServer();
+    }
 
     @Test
     public void test() {
         assertTrue(true);
     }
 
+    @Test
+    public void repoExists() {
+        JSONObject jsonObject = new JSONObject();
+        JSONObject repository = new JSONObject();
+        jsonObject.put("repository", repository);
+        repository.put("clone_url", "https://test.se");
+        jsonObject.put("branch", "testbranch");
+        File clonedRepoFile = new File("src/main/resources/");
+
+        //assertTrue(ContinuousIntegrationServer.cloneRepository(jsonObject, clonedRepoFile));
+    }
+    
+    @Test
+    public void okIfPush() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("eventType", "push");
+
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+ 
+        System.setOut(new PrintStream(outContent));
+        CIServer.handlePushEvent(jsonObject);
+        assertEquals("Event is 'push' - Proceeding with CI job\n", outContent.toString());
+    }
+
+    @Test
+    public void notOkIfNotPush() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("eventType", "pull");
+
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+ 
+        System.setOut(new PrintStream(outContent));
+        CIServer.handlePushEvent(jsonObject);
+        assertEquals("Not performing CI job - Event is not 'push'\n", outContent.toString());
+    }
+    //payloden innehåller nyckelorden
+
+    /*Status checkar: pending när du klonat ett repo innan du bygger
+    //Failure/success när du byggt
+    success skickas till GitHub*/
+
+    @Test
+    public void deletesAllFilesIfTheyExist(){
+        File f = new File("testFile");
+
+        CIServer.deleteDirectory(f);
+
+        assertNull("Successfully deleted file", f);
+    }
 }
