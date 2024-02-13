c8dffd2f53f2c2584f15d802fe5b103738b6e2d2
Tomas <73959718+tywe00@users.noreply.github.com>
2024-02-13 23:44:20 +0100

    6 notification (#11)
    * feat(notification): add code for commit status to API
    
    - add code for each commit status : pending, success, error and failure
    - add premilinary tests
    
    Closes #6
    
    * test: add test for commit status methods
    
    - Modify and add test cases for the commit status methods
    - Refactor the commit status methods to adjust for testing
    
    Closes #6
    
    * docs: documented all methods and constructors used
    
    ---------
    
    Co-authored-by: Tomas <73959718+tywe98@users.noreply.github.com>
    Co-authored-by: M1l0d <101342857+M1l0d@users.noreply.github.com>

---
+++ src/main/java/CommitStatus.java
@@ -0,0 +1,123 @@
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+public class CommitStatus {
+
+    private String owner = "M1l0d";
+    private String repo = "CI-DD2480-Group13";
+    private String BASEURL = "https://api.github.com/repos/";
+    private String sha;
+    private String targetUrl;
+    private CloseableHttpClient httpClient;
+    private String token;
+
+    /**
+     * Constructor for CommitStatus
+     * 
+     * @param sha       - The commit SHA
+     * @param targetUrl - The URL to the CI server
+     */
+    public CommitStatus(String sha, String targetUrl) {
+        this.sha = sha;
+        this.targetUrl = targetUrl;
+        this.token = "";
+        this.httpClient = HttpClients.createDefault();
+    }
+
+    /**
+     * Constructor for CommitStatus
+     * 
+     * @param sha        - The commit SHA
+     * @param targetUrl  - The URL to the CI server
+     * @param httpClient - The HTTP client to use for sending the status
+     */
+    public CommitStatus(String sha, String targetUrl, CloseableHttpClient httpClient) {
+        this.sha = sha;
+        this.targetUrl = targetUrl;
+        this.token = "";
+        this.httpClient = httpClient;
+    }
+
+    /**
+     * Method to send the commit status
+     * 
+     * @param state       - The state of the commit
+     * @param description - The description of the commit
+     */
+    public void sendCommitStatus(String state, String description) {
+        String url = BASEURL + owner + "/" + repo + "/statuses/" + sha;
+
+        String context = "CI server : Maven Build";
+
+        String jsonBody = "{\"state\": \"" + state + "\", \"target_url\": \"" + targetUrl
+                + "\", \"description\": \""
+                + description + "\", \"context\": \"" + context + "\"}";
+
+        HttpPost request = new HttpPost(url);
+        request.setHeader(HttpHeaders.AUTHORIZATION, "token " + token);
+        request.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+        request.setHeader(HttpHeaders.ACCEPT, "application/vnd.github.v3+json");
+        request.setHeader("X-GitHub-Api-Version", getDateandTime());
+        request.setEntity(new StringEntity(jsonBody, "UTF-8"));
+
+        try {
+            CloseableHttpResponse response = httpClient.execute(request);
+            System.out.println(response.getStatusLine().getStatusCode()); // Optional: Print response for debugging
+            HttpEntity entity = response.getEntity();
+            // Consume response body to release connection
+            EntityUtils.consume(entity);
+            response.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Set the commit status to pending
+     */
+    public void setCommitStatusToPending() {
+        sendCommitStatus("pending", "Commit validation is in progress.");
+    }
+
+    /**
+     * Set the commit status to success
+     */
+    public void setCommitStatusToSuccess() {
+        sendCommitStatus("success", "Build Successful: All tests passed.");
+    }
+
+    /**
+     * Set the commit status to failure
+     */
+    public void setCommitStatusToFailure() {
+        sendCommitStatus("failure", "Failure: Tests did not pass.");
+    }
+
+    /**
+     * Set the commit status to error
+     */
+    public void setCommitStatusToError() {
+        sendCommitStatus("error", "Error: Failed to build/test/verify the commit.");
+    }
+
+    /**
+     * Get the current date and time
+     * 
+     * @return current date and time
+     */
+    private String getDateandTime() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        return dtf.format(now);
+    }
+}
